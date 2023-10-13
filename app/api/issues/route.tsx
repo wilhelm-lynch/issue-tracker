bd33047a3dd8bd069822117163e189ae7c0cf257
Wilhelm Lynch <wilhelm.lynch@gmail.com>
2023-10-13 15:04:10 +0200

    Handle errors.

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -4,8 +4,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z.string().min(1, "Description is required"),
 });
 
 export const POST = async (request: NextRequest) => {
@@ -13,7 +13,7 @@ export const POST = async (request: NextRequest) => {
   const validation = createIssueSchema.safeParse(body);
 
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, {
+    return NextResponse.json(validation.error.format(), {
       status: status.BAD_REQUEST,
     });
 
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import SimpleMDE from "react-simplemde-editor";
-import { Button, TextField } from "@radix-ui/themes";
-import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
-import "easymde/dist/easymde.min.css";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { CrossCircledIcon } from "@radix-ui/react-icons";
+import "easymde/dist/easymde.min.css";
 
 interface IssueForm {
   title: string;
@@ -15,30 +17,45 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { control, register, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl space-y-5"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input
-          placeholder="Title"
-          {...register("title")}
-        ></TextField.Input>
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field}></SimpleMDE>
-        )}
-      />
-      <Button>Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Icon>
+            <CrossCircledIcon />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-5"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occured.");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input
+            placeholder="Title"
+            {...register("title")}
+          ></TextField.Input>
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field}></SimpleMDE>
+          )}
+        />
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
