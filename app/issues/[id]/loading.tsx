33c5e71ce7e8d7300105b800ff13e6d8c3f8b210
Wilhelm Lynch <wilhelm.lynch@gmail.com>
2023-10-14 23:09:46 +0200

    Refactor: Organize import statements.

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,8 +1,5 @@
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import { Box, Card, Flex, Heading, Text } from "@radix-ui/themes";
-import ReactMarkdown from "react-markdown";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Skeleton } from "@/app/components";
+import { Box, Card, Flex } from "@radix-ui/themes";
 
 const LoadingIssueDetailPage = () => {
   return (
